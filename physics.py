da6ab2ca0ca662bbb3fdb4d542beead688dfc416
PurposeinthePause <alonzo.mozee@gmail.com>
2017-02-09 18:13:11 -0500

    add sim files/template

---
+++ physics.py
@@ -0,0 +1,16 @@
+from pyfrc import drivetrains
+import math
+import wpilib
+
+class PhysicsEngine:
+
+    '''
+        Dictates how the robot will work in sim
+
+    '''
+    def __init__(self, controller):
+        pass
+
+
+    def update_sim(self, hal_data, now, tm_diff):
+        pass
