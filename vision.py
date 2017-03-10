4fd43ff33336ba633f1761a5f6277367e2b07e51
ofnlut <alonzo.mozee@gmail.com>
2017-03-10 00:34:14 -0500

    Add vision settings

--- vision.py
+++ vision.py
@@ -1,10 +1,17 @@
-from cscore import CameraServer
+from cscore import CameraServer, VideoSource
 
+"""
+Takes in camera via usb source on roborio and
+shows us video from two cameras in optimal settings
+"""
 def main():
     cs = CameraServer.getInstance()
     cs.enableLogging()
 
     usb1 = cs.startAutomaticCapture(dev=0)
+    usb1.setVideoMode(cs.VideoMode.PixelFormat.kMJPEG, 320, 240, 20)
+
     usb2 = cs.startAutomaticCapture(dev=1)
+    usb2.setVideoMode(cs.VideoMode.PixelFormat.kMJPEG, 320, 240, 20)
 
     cs.waitForever()
