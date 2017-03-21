9c4c3e70b3af49728445ea71faf25b6d98e0e86f
Devin Mozee <alonzo.mozee@gmail.com>
2017-03-21 17:39:11 -0400

    fixed issues around auto turning

--- autonomous/spin.py
+++ autonomous/spin.py
@@ -1,5 +1,8 @@
 from robotpy_ext.autonomous import StatefulAutonomous, timed_state
 from components import drive
+from robotpy_ext.common_drivers import navx
+
+
 
 from magicbot import AutonomousStateMachine, tunable, timed_state
 class MoveForward(AutonomousStateMachine):
@@ -13,24 +16,27 @@ class MoveForward(AutonomousStateMachine):
     '''
 
     #for dashboard and chooser
-    MODE_NAME = 'Move Forward'
+    MODE_NAME = 'Spin'
     Default = False
 
     drive = drive.Drive
+    navX = navx.AHRS
+
+    origin_yaw = 1
+    first_run = True
 
     def initialize(self):
         #here we can asign variables for the dashboard
         pass
 
-
-
-    @timed_state(duration=6,next_state='stop', first=True)
-    def go_foward(self):
-        try:
-            self.drive.arcade(0.5, 0)
-        except:
-            if not self.isFmsAttached():
-                raise
-    @timed_state(duration=5)
-    def stop(self):
-        self.drive.arcade(0,0)
+    @timed_state(duration=5, first=True, next_state="clean_up")
+    def turn(self):
+        if self.first_run:
+            self.origin_yaw = self.drive.navX.getYaw()
+            self.first_run = False
+        if not (self.drive.navX.getYaw() >= self.origin_yaw + 55):
+            self.drive.arcade(0, .5)
+    @timed_state(duration=1, first=False)
+    def clean_up(self):
+        self.first_run = True
+        print (self.drive.navX.getYaw(), " ", self.origin_yaw)
